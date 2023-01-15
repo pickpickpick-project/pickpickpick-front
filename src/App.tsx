115f79554c6ac872373e9207e5cf8d3c7516db25
HJ Kim <pknojlh27@naver.com>
2023-01-15 13:50:17 +0900

    feat: 메인화면 구조

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Main from "./pages/Home/Main";
 
 function App() {
-  return <div className="App">pickpickpick</div>;
+  return (
+    <div className="App" style={{ margin: "-8px" }}>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
