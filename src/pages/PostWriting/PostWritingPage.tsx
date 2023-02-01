52327188fe19fea04f9feb40056b6f133399c69f
jeongdopark <jdpark0816@gmail.com>
2023-02-01 15:41:51 +0900

    Feat: 문의게시판 글쓰기 페이지

---
+++ src/pages/PostWriting/PostWritingPage.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { PageStyled } from "../../assets/pageStyle";
+import Title from "../../assets/title";
+import colors from "../../assets/colors";
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+
+import CommonYellowButton from "../../components/Common/Button";
+const PostWritingPageStyle = styled(PageStyled)`
+    .button-wrapper{
+        display: flex;
+        justify-content: end;
+        margin-top : 30px;
+    }
+`
+
+const PostTitleInputWrapper = styled.div`
+    margin : 30px 0;
+    display : flex;
+    input{
+        border: none;
+        font-size : 32px;
+        &:focus {
+            outline : none;
+        }
+    }
+
+`
+
+
+const PostWritingPage = () => {
+    return(
+        <PostWritingPageStyle>
+            <PostTitleInputWrapper>
+                <input placeholder="문의글 제목"/>
+            </PostTitleInputWrapper>
+            <Editor
+                initialValue="문의 내용"
+                previewStyle="vertical"
+                height="600px"
+                initialEditType="markdown"
+                useCommandShortcut={true}
+            />
+            <div className="button-wrapper">
+                <CommonYellowButton width={200} height={50} hover={false} text={"제출하기"} onClick={() => {}}/>
+            </div>
+        </PostWritingPageStyle>
+    )
+}
+
+export default PostWritingPage;
