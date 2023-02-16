a6c7fdfd33fa0325fb70bf745df5389c8a87525c
jeongdopark <jdpark0816@gmail.com>
2023-02-16 18:15:47 +0900

    Fix : CORS error 해결

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "ec2-52-79-45-14.ap-northeast-2.compute.amazonaws.com:8080/",
+	baseURL: "http://ec2-15-164-113-99.ap-northeast-2.compute.amazonaws.com:8080/",
 	headers: {
 		'Content-type': 'application/json; charset=UTF-8',
 		accept: 'application/json,',
