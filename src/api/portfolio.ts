bc9862f628b68aa442ad76a1a4111c8e2a225549
jeongdopark <jdpark0816@gmail.com>
2023-02-16 18:28:45 +0900

    Feat : API 불러오기

--- src/api/portfolio.ts
+++ src/api/portfolio.ts
@@ -1,8 +1,11 @@
 import api from "./api";
-import { Portfolio } from "./types";
 
-
-export async function getUserPortfolioImgs(userNum: number){
-    const response = await  api.get<Portfolio>(`portfolio?userNum=${userNum}`);
+export async function getUserPortfolio(userNum: number){
+    const response = await  api.get(`portfolio/list/user/${userNum}`);
     return response.data;
+}
+
+export async function getPortfolioInfo(portfolioNumber:number){
+    const response = await api.get(`portfolio/${portfolioNumber}`);
+    return response.data.data
 }
