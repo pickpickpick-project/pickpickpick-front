d160a2d679eb7a14e008565fa28fb3ad39d87213
jeongdopark <jdpark0816@gmail.com>
2023-02-27 11:32:41 +0900

    Feat : 마이페이지 결제내역

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
 import colors from "../assets/colors";
 import MypageProfile from "../components/Mypage/Mypage_profile";
 import CommonCarousel from "../components/Common/Carousel";
 import { CommonText } from "../components/Artist/ArtistStyled";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { getUserPortfolio } from "../api/portfolio";
 import { getWorkList } from "../api/work";
 import { getUserInfo } from "../api/user";
+import { getOrderList, getOrderStatus, handlePaymentCancel } from '../api/order';
+import { Pagination } from "@mui/material";
+import { Paging, Table } from "../pages/Admin/ManageUserPage";
+import axios from 'axios';
+
 
 const MypageStyled = styled.div`
     padding: 125px 16px 140px 16px;
@@ -19,6 +24,7 @@ const MypageStyled = styled.div`
     color: ${colors.text};
 `
 
+
 interface test{
     portfolio_id : number,
     user_id : number,
@@ -26,23 +32,37 @@ interface test{
     portfolio_type : number | string,
 }
 
+
 interface props{
     portfolioProps : test[];
 }
+
  
 const Mypage = ():React.ReactElement => {
 
-    const [ productImageList, setProductImageList ] = useState<any>([]);
-    const [ portfolioImageList, setPortfolioImageList ] = useState<any>([])
-
+    
+    
+    // const [ merchantUid, setMerchantUid ] = useState<string>(''); // useState -> 값을 즉시 변동 못해서 전역 변수로 설정함. 리팩토링 필요
+    
+    const merchantUid = useRef('');
+    const orderStatus = useRef('');
+    const cancelPrice = useRef(0);
     const userId = Number(localStorage.getItem('userId'));
     let user_email = localStorage.getItem('email');
     const getProductListData = useQuery("getWorkList", () => getWorkList(userId));
     const getPortfolioData = useQuery("getPortfolioList", () => getUserPortfolio(userId));    
     const { data : User } = useQuery("getUser", () => getUserInfo(userId));
+    const { data : OrderList, refetch } = useQuery(["getOrder", orderStatus.current], () => getOrderList(userId));
+    console.log(OrderList);
+    
+    const count = Math.ceil(OrderList.data.data.length / 4);
+    const  [ page, setPage ] = useState(1);
+    const [ pageItems, setPageItems ] = useState(OrderList.data.data.slice(0, 4));
+    console.log(OrderList);
     
     const productListCount = getProductListData.data?.length;
     const portfolioListCount = getPortfolioData.data?.data.length;
+
     
     let productArray = [];
     let portfolioArray = [];
@@ -52,7 +72,70 @@ const Mypage = ():React.ReactElement => {
     for(let i=0; i<portfolioListCount; i++){
         portfolioArray.push(getPortfolioData.data.data[i])
     }
+
+
+    const { mutate : cancel } = useMutation(handlePaymentCancel, {
+        onSuccess : data => {
+            console.log(data);
+            orderStatus.current = "CANCEL";
+
+            axios.get('http://ec2-15-164-113-99.ap-northeast-2.compute.amazonaws.com:8080/orders/status/',{
+            params:{
+                merchantUid:merchantUid.current,
+                orderStatus:orderStatus.current,
+            }
+            })
+            .then((res) => {
+                refetch()
+                console.log(res);
+            })
+            .catch((res) => console.log(res));
+        },
+        onError : data => {
+
+        }
+    })
+
+    const onClickOrderStatusComplete = (item:any) => {
+        orderStatus.current = "COMPLETE";
+        merchantUid.current = item.merchantUid;
+
+        axios.get('http://ec2-15-164-113-99.ap-northeast-2.compute.amazonaws.com:8080/orders/status/',{
+            params:{
+                merchantUid:merchantUid.current,
+                orderStatus:orderStatus.current,
+            }
+            })
+            .then((res) => refetch())
+            .catch((res) => console.log(res));
+    }
+
+    const onClickOrderStatusCancel = (item:any) => {
+        cancelPrice.current = item.orderPrice;
+        merchantUid.current = item.merchantUid;
+        
+        cancel({
+            merchant_uid:merchantUid.current,
+            cancel_request_amount:cancelPrice.current,
+        })
+    }
+
+    const handlePage = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+        setPageItems(OrderList.data.data.slice(4 * (value - 1), 4 * value));
+      };
+
+      useEffect(() => {
+        setPageItems(OrderList.data.data.slice(0, 4));
+      }, [OrderList.data.data]);
+
+    useEffect(() => {
+    }, [merchantUid.current])
+
+    useEffect(() => {  
+    }, [orderStatus.current])
     
+
     return (
         getPortfolioData.status === 'loading' || getProductListData.status === 'loading'?
         <div>loadingloadingloadingloadingloading</div> :
@@ -62,6 +145,47 @@ const Mypage = ():React.ReactElement => {
             <CommonCarousel data={portfolioArray} category={'portfolio'}/> 
             <CommonText>판매</CommonText>
             <CommonCarousel data={productArray} category={'work'}/>
+            <CommonText>구매 이력</CommonText>
+            <Table>
+              <table>
+                <thead>
+                  <tr>
+                    <th className="num">주문 번호</th>
+                    <th>상품 이름</th>
+                    <th>수량</th>
+                    <th>가격</th>
+                    <th>주문 상태</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {OrderList !== undefined ?
+                    pageItems.map((item: any, index: number) => (
+                        <tr key={index}>
+                        <td>{item.orderNum}</td>
+                        <td>{item.workName}</td>
+                        <td>{item.orderCount}</td>
+                        <td>{item.workPrice}</td>
+                        <td>{
+                                item.orderStatus === "CONFIRM" 
+                                ? <div style={{display:"flex", alignItems:"center"}}>
+                                    <h3>결제 완료</h3>
+                                    <button onClick={() => onClickOrderStatusComplete(item)}>구매 확정</button>
+                                    <button onClick={() => onClickOrderStatusCancel(item)}>구매 취소</button>
+                                </div>
+                                :   item.orderStatus === "COMPLETE" ? "구매확정" 
+                                : "구매취소" 
+                                }</td>
+                        </tr>
+                    )) : 
+                    <div>loading</div>
+                }
+                </tbody>
+                </table>
+            </Table>
+              <Paging>
+                <Pagination count={count} page={page} onChange={handlePage}/>
+              </Paging>
             {/* <CommonText>구매</CommonText>
             <CommonCarousel data={portfolioData}/> */}
         </MypageStyled>
